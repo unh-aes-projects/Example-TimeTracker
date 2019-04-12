a050b0390a2ae7d4d644f7e6a268c934c18f2542
carpepraedam <jax472unh@gmail.com>
2019-04-12 14:33:10 -0400

    a project now has a 1-to-many relationship with user, timeevent has a 1-to-many relationship with project

--- src/timetracker/models/project_model.py
+++ src/timetracker/models/project_model.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Project(models.Model):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- src/timetracker/models/time_event_model.py
+++ src/timetracker/models/time_event_model.py
@@ -1,12 +1,11 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class TimeEvent(models.Model):
     start_datetime = models.DateTimeField()
     end_datetime = models.DateTimeField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    project_id = models.ForeignKey(
+    comments = models.CharField(max_length=255)
+    project = models.ForeignKey(
         'Project',
         on_delete=models.CASCADE,
         null=True)
